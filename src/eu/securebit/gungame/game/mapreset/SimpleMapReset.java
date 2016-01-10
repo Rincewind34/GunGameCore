dae8d1cbe7428aebd1078ca8b6b8995201f3a1a0
Janhektor <hektorabc@gmail.com>
2016-01-10 16:11:25 +0100

    [+] MapReset

---
+++ src/eu/securebit/gungame/game/mapreset/SimpleMapReset.java
@@ -0,0 +1,77 @@
+package eu.securebit.gungame.game.mapreset;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.material.MaterialData;
+
+public class SimpleMapReset implements MapReset {
+
+	private final List<World> worlds;
+	private final Map<Location, MaterialData> saves;
+	
+	private boolean recording;
+	
+	public SimpleMapReset() {
+		this.worlds = new ArrayList<>();
+		this.saves = new HashMap<>();
+		this.recording = false;
+	}
+	
+	@Override
+	public void add(World world) {
+		this.worlds.add(world);
+	}
+
+	@Override
+	public void remove(World world) {
+		this.worlds.remove(world);
+	}
+
+	@Override
+	public void startRecord() {
+		this.saves.clear();
+		this.recording = true;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void breakBlock(Location location, Block original) {
+		if (this.recording && this.worlds.contains(location.getWorld())) {
+			this.saves.put(location, new MaterialData(original.getType(), original.getData()));
+		}
+	}
+
+	@Override
+	public void placeBlock(Location location) {
+		if (this.recording && this.worlds.contains(location.getWorld())) {
+			this.saves.put(location, new MaterialData(Material.AIR));
+		}
+	}
+
+	@Override
+	public int discard() {
+		int count = this.saves.size();
+		this.saves.clear();
+		this.recording = false;
+		return count;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public int rollback() {
+		int count = this.saves.size();
+		this.saves.forEach((location, data) -> {
+			location.getBlock().setType(data.getItemType());
+			location.getBlock().setData(data.getData());
+		});
+		this.recording = false;
+		return count;
+	}
+}
